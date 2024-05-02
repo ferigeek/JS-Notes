29eaca90ff07cd9d563ed9cd43cf9111bba0aead
ferigeek <150931210+ferigeek@users.noreply.github.com>
2024-05-02 11:43:18 +0330

    Imoji

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "JavaScript Notes",
+    pageTitle: "💻JavaScript Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
